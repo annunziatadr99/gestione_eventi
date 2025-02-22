6ce90b74e1249866443d043669ca1d98f2bc721f
annunziatadr99 <anudar99@hotmail.it>
2025-02-22 02:57:43 +0100

    Create classi payload per login, registrazione e classe JwtResponse.Procederò domani per l'implementazione della Security

---
+++ src/main/java/com/demo/gestione_eventi/payload/request/RegistrazioneRequest.java
@@ -0,0 +1,30 @@
+package com.demo.gestione_eventi.payload.request;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+import java.util.Set;
+
+@Data
+public class RegistrazioneRequest {
+
+    @NotBlank
+    @Size(min = 3, max = 15)
+    private String username;
+
+    @NotBlank
+    @Size(min = 3, max = 20)
+    private String password;
+
+    @NotBlank
+    private String cognome;
+
+    @NotBlank
+    @Email
+    private String email;
+
+    private Set<String> ruoli;
+}
+
