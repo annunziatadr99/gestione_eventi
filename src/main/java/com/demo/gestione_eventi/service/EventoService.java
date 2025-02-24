5aca0a913e9b22298f59614fe62b5b7548389b7e
annunziatadr99 <anudar99@hotmail.it>
2025-02-24 16:09:11 +0100

    Grazie a Dio il progetto funziona, quindi posso dire PROGETTO COMPLETATO

--- src/main/java/com/demo/gestione_eventi/service/EventoService.java
+++ src/main/java/com/demo/gestione_eventi/service/EventoService.java
@@ -9,7 +9,6 @@ import com.demo.gestione_eventi.repository.UtenteRepository;
 import com.demo.gestione_eventi.payload.request.EventoRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import java.util.List;
 import java.util.Optional;
 
